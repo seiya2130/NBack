b46da5063829c39bca90b44e090afa53c32ecd4f
Seiya_Satoh <satohseiya0312@gmail.com>
2022-08-16 09:00:51 +0900

    設定ページ追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SettingPageComponent } from './page/setting-page/setting-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingPageComponent
   ],
   imports: [
     BrowserModule,
