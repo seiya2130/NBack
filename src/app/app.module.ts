dee1dfaab184e83e8abbacd20d497f27880f5a49
Seiya_Satoh <satohseiya0312@gmail.com>
2022-08-18 09:00:04 +0900

    sectionコンポーネント追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { SettingPageComponent } from './page/setting-page/setting-page.component
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ForwardButtonComponent } from './ui/gui/button/forward-button/forward-button.component';
 import { MaterialModule } from './material.module';
+import { SectionComponent } from './ui/layout/section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SettingPageComponent,
     ForwardButtonComponent,
+    SectionComponent,
   ],
   imports: [
     BrowserModule,
