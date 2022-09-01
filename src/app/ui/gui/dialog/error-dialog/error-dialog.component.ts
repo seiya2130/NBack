6cb9e453b7ab2d2caaf84360698bb166937fd20d
Seiya_Satoh <satohseiya0312@gmail.com>
2022-09-01 09:18:58 +0900

    エラーダイアログ追加

---
+++ src/app/ui/gui/dialog/error-dialog/error-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-error-dialog',
+  templateUrl: './error-dialog.component.html',
+  styleUrls: ['./error-dialog.component.scss']
+})
+export class ErrorDialogComponent implements OnInit {
+
+  constructor(
+    private dialogRef: MatDialogRef<ErrorDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string[],
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  close(): void {
+    this.dialogRef.close();
+  }
+}
